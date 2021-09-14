b5fea34eceec8deb2ad7b0d7e91fa542d6469806
Julian Tran <juliantran003@gmail.com>
2021-09-14 14:49:55 +0100

    promoBanner updated

--- src/components/Promotions/PromotionalBanner.js
+++ src/components/Promotions/PromotionalBanner.js
@@ -3,13 +3,11 @@ const PromotionalBanner = ({ onX, onContact }) => {
     <div className="promotionBanner">
       <div className="promotionBanner__container">
         <i class="fas fa-times promotionBanner__x " onClick={onX}></i>
-        <h1 className="promotionBanner__message">
-          10% off for New Customers !
-        </h1>
+        <h1 className="promotionBanner__message">10% off for new customers</h1>
         <h2 className="promotionBanner__instructions">
-          Enter <span>PROMO111</span> as Reference Number when Contacting us
+          enter <span>PROMO111</span> reference number
         </h2>
-        <button className="btn-classic" onClick={onContact}>
+        <button className="promotionBanner__btn" onClick={onContact}>
           Contact
         </button>
       </div>
