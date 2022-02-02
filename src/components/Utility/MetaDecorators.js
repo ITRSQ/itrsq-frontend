ba1052202bc8f4d79c4dedfcf67a13139aa43cd5
anchor <30292770+AnchorTaken@users.noreply.github.com>
2022-02-02 11:00:48 +0000

    Dynamic Blog Hotfix
    > Added Dynamic Blog Func

---
+++ src/components/Utility/MetaDecorators.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { PropTypes } from "prop-types";
+import { Helmet } from 'react-helmet';
+
+const MetaDecorator = ({title, description, tags, image}) => {
+    return (
+    <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={tags}></meta>
+        <meta property="og:image" content={image} />
+    </Helmet>
+    )
+};
+
+MetaDecorator.propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+};
+
+export default MetaDecorator;
